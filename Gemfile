ed405ede51df95eb70111a08bc8f0d3e61a4cd1a
susancorpuz <susan.corpuz@gmail.com>
2020-02-28 22:56:22 +0800

    mostly made changes in stylesheet
    Signed-off-by: susancorpuz <susan.corpuz@gmail.com>

--- Gemfile
+++ Gemfile
@@ -6,11 +6,20 @@ ruby '2.7.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.4'
+
+#gem 'sqlite3', '~> 1.4'
+group :development do
+  gem 'sqlite3'
+end
+group :production do
+  gem 'pg'
+end
+
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'devise'
 gem 'gravtastic'
+#gem 'bootstrap', '~> 4.4.1'
 
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Idea, type: :model do
+  it "has a name" do # yep, you can totally use 'it'
+    idea = Idea.create!(name: "My Awesome Idea Name") # creating a new idea 'instance'
+    second_idea = Idea.create!(name: "My Second Idea Name") # creating another new idea 'instance'
+    expect(second_idea.name).to eq("My Second Idea Name") # this is our expectation
+  end
+
+  it "has a description" do
+    description = Idea.create!(name: "Whatever will be will be")
+    second_description = Idea.create!(name: "OMG! It's actually working")
+    expect(second_description.name).to eq("OMG! It's actually working")
+  end
+
+end
