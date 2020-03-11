b931e8e0b2ae6028c499b70f8e32c5e897f67adb
susancorpuz <susan.corpuz@gmail.com>
2020-03-11 18:41:53 +0800

    make it better

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,9 @@
 require "rails_helper"
-
+require "ideas"
+# REVIEW: has associative value with Idea
 RSpec.describe Comment, type: :model do
-  it "has user name" do # yep, you can totally use 'it'
-    skip
+  it "has a name" do # yep, you can totally use 'it'
+    user_name = Comment.create!(user_name: "Pokwang")
+    expect(user_name).to eq("Chocolate")
   end
 end
