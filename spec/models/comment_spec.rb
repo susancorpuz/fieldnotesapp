ca50d676987657e8cc256c548fe5d24ace427e3b
susancorpuz <susan.corpuz@gmail.com>
2020-03-12 00:08:40 +0800

    fixed gem install pg
    Signed-off-by: susancorpuz <susan.corpuz@gmail.com>

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,6 @@ require "ideas"
 # REVIEW: has associative value with Idea
 RSpec.describe Comment, type: :model do
   it "has a name" do # yep, you can totally use 'it'
-    user_name = Comment.create!(user_name: "Pokwang")
-    expect(user_name).to eq("Chocolate")
+    comment.created?.should be true
   end
 end
