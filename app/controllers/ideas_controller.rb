275dac0452542bf9e9bb8bbe2425be72e64056c8
susancorpuz <susan.corpuz@gmail.com>
2020-03-13 19:02:54 +0800

    format document
    Signed-off-by: susancorpuz <susan.corpuz@gmail.com>

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class IdeasController < ApplicationController
-  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+  before_action :set_idea, only: %i[show edit update destroy]
 
   # GET /ideas
   # GET /ideas.json
@@ -21,8 +23,7 @@ class IdeasController < ApplicationController
   end
 
   # GET /ideas/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /ideas
   # POST /ideas.json
@@ -65,13 +66,14 @@ class IdeasController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
-      @idea = Idea.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def idea_params
-      params.require(:idea).permit(:name, :description, :picture, :created_at)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def idea_params
+    params.require(:idea).permit(:name, :description, :picture, :created_at)
+  end
 end
